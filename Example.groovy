1ca827f82237cd6dfa0d28464a1c434f6deb5822
rizsac <42241570+rizsac@users.noreply.github.com>
2018-08-14 09:31:45 -0400

    Add files via upload

---
+++ Example.groovy
@@ -0,0 +1,11 @@
+def exampleMethod() {
+    Set<Object> configs = [] as Set<Object>
+    configs << "exampleMethod"
+    configs << "exampleMethod1"
+    return configs
+}
+
+def otherExampleMethod() {
+    println("otherExampleMethod")
+}
+return this
